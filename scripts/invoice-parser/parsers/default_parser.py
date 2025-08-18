610c2e03622a8dc195970e01395b03a5cd868eb3
jonathanHas <jhaslamster@gmail.com>
2025-08-18 22:45:29 +0100

    removed scripts/invoice-parser

--- scripts/invoice-parser/parsers/default_parser.py
+++
@@ -1,9 +0,0 @@
-def parse_invoice(text, filename):
-    return {
-        'Filename': filename,
-        'Supplier': 'Unknown',
-        'Total': 'Not found',
-        'Invoice Date': 'Not found',
-        'Tax Free': False,
-        'Credit Note': False
-    }
